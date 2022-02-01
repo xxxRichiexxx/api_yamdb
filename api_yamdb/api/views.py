8cad714141c0905578148461c23d1766a96ba713
Lev Khalyapin <lev.kh@yandex.ru>
2022-02-01 13:49:10 +0300

    Добавлены вьюсеты категорий, жанров и произведений

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,30 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
+from reviews.models import Category, Genre, Title
+
+from .permissions import IsAdminOrReadOnly
+from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
+
+
+class CatigoriesViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ('name',)
+
+
+class GenresViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ('name',)
+
+
+class TitlesViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('category__slug', 'genre__slug', 'name', 'year')
