2e2bf3a8ef3598d63a381a93c60f9094a8af3f80
Lev Khalyapin <lev.kh@yandex.ru>
2022-02-03 22:35:34 +0300

    Новый пермишн IsAdminOrSuperUserOrReadOnly

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,9 +1,13 @@
 from rest_framework import permissions
 
 
-class IsAdminOrReadOnly(permissions.BasePermission):
-    message = 'Пользователь не является администратором!'
+class IsAdminOrSuperUserOrReadOnly(permissions.BasePermission):
+    message = (
+        'Пользователь не является администратором '
+        'или супер-пользователем!'
+    )
 
     def has_object_permission(self, request, view, obj):
-        return (request.user.is_staff
+        return ((request.user.is_staff
+                or request.user.is_superuser)
                 or request.method in permissions.SAFE_METHODS)
